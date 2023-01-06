8de13caa5011cab8295e52855ab2395aa68bfcd2
Paul Wagner <p.wagner1120@icloud.com>
2023-01-06 11:04:37 -0600

    adding bonus items to HighLow game

--- src/HighLow.java
+++ src/HighLow.java
@@ -8,17 +8,37 @@ public class HighLow {
 
         System.out.println(randomNum);
 
+
+        System.out.println("Welcome to HighLow! You have 5 attempts to pick the right number! Good Luck!");
         System.out.println("Enter a number between 1 & 100: ");
         int userInput;
+        int userAttempts = 0;
 
         do {
             userInput = scanner.nextInt();
             if (userInput < randomNum) {
                 System.out.println("HIGHER");
+                userAttempts++;
+                System.out.println("You have made " + userAttempts + " attempts.");
+                    if (userAttempts == 5){
+                        System.out.println("You have reached the maximum number of attempts!");
+                        break;
+                    }
             } else if (userInput > randomNum) {
                 System.out.println("LOWER");
+                userAttempts++;
+                System.out.println("You have made " + userAttempts + " attempts.");
+                    if (userAttempts == 5){
+                        System.out.println("You have reached the maximum number of attempts!");
+                        break;
+                    }
             }
         } while(userInput != randomNum);
-            System.out.println("GREAT GUESS!");
+            if(userInput == randomNum){
+                System.out.println("Congrats! You have chosen the correct number!!!");
+            } else {
+                System.out.println("Thanks for Playing!");
+            }
+
     }
 }
