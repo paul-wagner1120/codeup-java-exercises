00898abf6352fcab1fff8e93e8b299ab70d1dd11
Paul Wagner <p.wagner1120@icloud.com>
2023-01-15 15:48:04 -0600

    adding some java practice

---
+++ src/GUI_java.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class GUI_java {
+    public static void main(String[] args) {
+
+        String name = JOptionPane.showInputDialog("Enter your name!");
+        JOptionPane.showMessageDialog(null,"Hello "+ name);
+
+
+
+
+
+
+    }
+
+
+}
---
+++ src/JavaPractice.java
@@ -0,0 +1,59 @@
+import java.util.Scanner; // importing java scanner
+
+public class JavaPractice {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in); // setting up scanner
+
+        // creating a variable
+
+//        int x; // declaration
+//
+//        x = 123; // assignment
+//
+//        int x = 123; //initialization
+//        double y = 3.14; //used for decimal numbers with more precision than float
+//        boolean z = true; // only accepts true or false
+//        char symbol = '@'; // storing a single character
+//        String name = "Paul"; // strings need double quotes
+//
+//        System.out.println(x); //printing to the console -- similar to console log
+
+        // swapping variables
+
+//        String xy = "water";
+//        String yz = "koolaid";
+//        String temp;
+//
+//        temp = xy;
+//        xy = yz;
+//        yz = temp;
+
+//        System.out.println("What is your name? "); // accepting user input
+//        String name = scanner.nextLine();  // capturing user input using the scanner feature // captures entire line
+//        System.out.println("How old are you? ");
+//        int age = scanner.nextInt(); //captures numbers
+//        System.out.println("What is your favorite food");
+//        String food = scanner.next(); //captures a single word of input
+//
+//        System.out.println("Hello " + name); // printing out a message + userInput
+//        System.out.println("You are " + age + " years old");
+//        System.out.println("You like "+ food);
+
+
+        // expressions in java
+
+        //expression = operands & operators
+        //operands = values, variables, numbers, quantity
+        //operator = +(add) -(subtract) *(multiply) /(divide) %(modulus = gives remainder)
+
+        int friends = 10;
+        friends = friends * 2;
+
+        friends++;
+
+
+
+    }
+}
